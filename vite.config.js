97c88134cfbeb8453b704e6947268c55b0e8e0ad
MyoLinTun <mgmyolintun@gmail.com>
2025-12-03 19:06:57 +0630

    changed port for localhost

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+            port: 3000, // Or any other available port
+          },
   plugins: [react(), tailwindcss()],
 })
